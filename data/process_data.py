db21cc32318fd43f56d4733932ac11350d4aad73
GlesgaJitter <98545760+GlesgaJitter@users.noreply.github.com>
2022-04-13 15:24:18 +0100

    Update process_data.py

--- data/process_data.py
+++ data/process_data.py
@@ -51,8 +51,8 @@ def clean_data(df):
     return df
 
 def save_data(df, database_filename):
-    engine = create_engine('sqlite:///messages_cleaned.db')
-    df.to_sql('messages_cleaned', engine, index=False)
+    engine = create_engine('sqlite:///'+database_filepath)
+    df.to_sql('messages_cleaned', engine, index=False, if_exists='replace')
 
 
 def main():
